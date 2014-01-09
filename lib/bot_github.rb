0e108d078a03555ed122e81ec04ba8df024d6e59
Tom Corwine <tc@corwine.org>
2014-01-09 11:17:12 -0500

    Changed order of status checking.

--- lib/bot_github.rb
+++ lib/bot_github.rb
@@ -45,16 +45,16 @@ class BotGithub
           # User triggered a new build by clicking Integrate on the Xcode server interface
           puts "Manually triggered on #{br.name}"
           create_status(br, github_state_new, convert_bot_status_to_github_description(bot), bot.status_url)
-        elsif (github_state_new != :unknown && github_state_cur != github_state_new)
-          # Build has passed or failed so update status and comment on the issue
-          #create_comment_for_bot_status(br, bot)
-          puts "Update status on #{br.name}"
-          create_status(br, github_state_new, convert_bot_status_to_github_description(bot), bot.status_url)
         elsif (github_state_cur == :unknown || user_requested_retest(br, bot))
           # Unknown state occurs when there's a new commit so trigger a new build
           puts "New commit on #{br.name}"
           BotBuilder.instance.start_bot(bot.guid)
           create_status_new_build(br)
+        elsif (github_state_new != :unknown && github_state_cur != github_state_new)
+          # Build has passed or failed so update status and comment on the issue
+          #create_comment_for_bot_status(br, bot)
+          puts "Update status on #{br.name}"
+          create_status(br, github_state_new, convert_bot_status_to_github_description(bot), bot.status_url)
         else
           puts "BR #{br.bot_long_name} (#{github_state_cur}) is up to date for bot #{bot.long_name}"
         end
