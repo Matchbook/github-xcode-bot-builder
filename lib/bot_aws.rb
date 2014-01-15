75939ab01e503c6ef16c36d5f39f843f0f0b4e58
Tom Corwine <tc@corwine.org>
2014-01-15 12:48:36 -0500

    Actually put mkdir line before the clone line.

--- lib/bot_aws.rb
+++ lib/bot_aws.rb
@@ -86,8 +86,8 @@ class BotAWS
       git = Git.open(git_local_path, :log => Logger.new(STDOUT))
       puts "opening repo #{git_repo_name}."
     else
-      git = Git.clone(temp_path, git_repo_name, :path => git_local_path)
       FileUtils.mkdir_p(temp_path)
+      git = Git.clone(temp_path, git_repo_name, :path => git_local_path)
       puts "cloning repo #{git_repo_name}."
     end
     git.branch(branch_name)
