cac3d7d366743c6b8ad77db6db1322cbad3903a3
Tom Corwine <tc@corwine.org>
2014-01-14 19:06:30 -0500

    More fixes.

--- lib/bot_aws.rb
+++ lib/bot_aws.rb
@@ -30,14 +30,39 @@ class BotAWS
       return
     end
 
-    if (s3_bucket.objects["#{file_name}.plist"].exists?)
-      return # Build already uploaded
-    end
-
     ipa_file_name = "/Library/Server/Xcode/Data/BotRuns/BotRun-#{bot.latestSuccessfulBotRunGUID}.bundle/output/#{bot.long_name}.ipa"
     if ( ! File.exists?(ipa_file_name))
       puts "File not uploaded. \"#{ipa_file_name}\" does not exist."
       return
+    end
+
+    extract_location = File.join('/', 'tmp', "#{Time.now.getutc.to_s}", 'Info.plist')
+    ZipFile.open(ipa_file_name) do |zf|
+      zf.each do |e|
+        if (e.name == "Info.plist")
+          zf.extract(e.name, extract_location)
+          break
+        end
+      end
+    end
+
+    if ( ! File.exists?(extract_location))
+      puts "Could not extract Info.plist from ipa."
+      return
+    end
+
+    plist_buddy_path = File.join('/', 'usr', 'libexec', 'PlistBuddy')
+    bundle_version_string = %x(#{plist_buddy_path} -c "Print CFBundleVersion" #{extract_location})
+    bundle_identifier = %x(#{plist_buddy_path} -c "Print CFBundleIdentifier" #{extract_location})
+    bundle_display_name = %x(#{plist_buddy_path} -c "Print CFBundleDisplayName" #{extract_location})
+
+    upload_display_name = BotConfig.instance.aws_upload_display_name(branch_name)
+    title = (upload_display_name ? upload_display_name : bundle_display_name)
+
+    file_name = "#{bundle_identifier}-#{bundle_version_string}"
+
+    if (s3_bucket.objects["#{file_name}.plist"].exists?)
+      return # Build already uploaded
     end
 
     git_url = BotConfig.instance.github_url
@@ -57,35 +82,9 @@ class BotAWS
     puts "Checking out commit #{last_commit_hash}"
     git.checkout(last_commit_hash)
 
-    extract_location = File.join('/', 'tmp', "#{Time.now.getutc.to_s}", 'Info.plist')
-    ZipFile.open(ipa_file_name) do |zf|
-      zf.each do |e|
-        if (e.name == "Info.plist")
-          zf.extract(e.name, extract_location)
-          break
-        end
-      end
-    end
-
-    if ( ! File.exists?(extract_location))
-      puts "Could not extract Info.plist from ipa."
-      return
-    end
-
-    plist_buddy_path = File.join('/', 'usr', 'libexec', 'PlistBuddy')
-    version_string = %x(#{plist_buddy_path} -c "Print CFBundleVersion" #{extract_location})
-    bundle_identifier = %x(#{plist_buddy_path} -c "Print CFBundleIdentifier" #{extract_location})
-    bundle_display_name = %x(#{plist_buddy_path} -c "Print CFBundleDisplayName" #{extract_location})
-
-    upload_display_name = BotConfig.instance.aws_upload_display_name(branch_name)
-    title = (upload_display_name ? upload_display_name : bundle_display_name)
-
-    key_prefix = "#{bundle_identifier}-#{version_string}"
-
     puts "Uploading #{title}..."
 
     # Upload ipa
-    file_name = title.sub(' ', '-')
     s3_bucket.objects["#{file_name}.ipa"].write(:file => ipa_file_name, :acl => :public_read)
     puts "Uploaded ipa for \"#{title}\" on branch \"#{branch_name}\" to bucket #{upload_bucket}."
 
@@ -97,7 +96,7 @@ class BotAWS
     plist_string = template.render(
       'ipa_url' => ipa_url,
       'bundle_identifier' => bundle_identifier,
-      'version_string' => version_string,
+      'version_string' => bundle_version_string,
       'title' => title
       )
     s3_bucket.objects["#{file_name}.plist"].write(plist_string, :acl => :public_read)
@@ -106,7 +105,6 @@ class BotAWS
     # Upload html file
     builds = []
     custom_file_name = BotConfig.instance.aws_upload_html_file_name(branch_name)
-    file_name = (custom_file_name ? custom_file_name : key_prefix)
     list_versions = BotConfig.instance.aws_upload_list_all_versions(branch_name)
 
     if (list_versions) # List each plist found in the bucket
